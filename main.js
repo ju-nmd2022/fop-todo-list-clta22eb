7b2d4e0064439141bd719cbc0addda402e0f26e9
clta22eb <clta22eb@student.ju.se>
2023-04-07 16:57:54 +0200

    add tasks

--- main.js
+++ main.js
@@ -1,28 +1,40 @@
-const tasks = document.getElementsByClassName("tasks");
-const articleElement = document.createElement("article");
-articleElement.classList.add("tasks");
-
-const taskElement = document.createElement("p");
-taskElement.innerHTML = "task";
-articleElement.appendChild(taskElement);
-
-// return articleElement;
+const tasksElement = document.getElementById("tasks");
+const addTaskElement = document.getElementById("add-task");
+const inputElement = document.getElementById("input");
+const buttonElement = document.getElementById("button");
 
 
-let newTask = "walk my fish";
-
-const addElement = document.getElementById("new-task");
-
-const inputElement = document.createElement("input");
-inputElement.placeholder = "Username";
-newTaskElement.appendChild(inputElement);
+function createList() {
+    buttonElement.addEventListener("click", addTask);
+}
 
 
-
-addButton.addEventListener("click", () => {
+function addTask() {
     if (inputElement.value.length > 0) {
-        currentUser = inputElement.value;
-        checkLogin();
-        displayPosts();
+        
+        const taskElement = document.createElement("div");
+            const task = document.createElement("span");
+            thingToDo = inputElement.value;
+            taskElement.innerText = thingToDo;
+            taskElement.appendChild(task);
+            taskElement.addEventListener("click", () => {
+                taskElement.classList.add("completed");
+            });
+        
+            const removeButton = document.createElement("button");
+            removeButton.classList.add("remove-button");
+            removeButton.innerText = "🚫";
+            removeButton.addEventListener("click", removeTask);
+            taskElement.appendChild(removeButton);
+        
+        tasksElement.appendChild(taskElement);
+    } else {
+        alert("Please enter a task!");
     }
-});
+}
+
+
+
+createList();
+
+
