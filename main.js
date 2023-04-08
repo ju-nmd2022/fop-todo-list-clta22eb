b432f254e173f187182478b51eba9e61389438a6
clta22eb <clta22eb@student.ju.se>
2023-04-08 18:14:22 +0200

    attempt 1 at localStorage = failed :(

--- main.js
+++ main.js
@@ -1,47 +1,45 @@
-const tasksElement = document.getElementById("tasks");
+const taskContainerElement = document.getElementById("tasks");
 const addTaskElement = document.getElementById("add-task");
 const inputElement = document.getElementById("input");
 const buttonElement = document.getElementById("button");
 
 
-function createList() {
-    buttonElement.addEventListener("click", addTask);
-}
-
-
-function addTask() {
+buttonElement.addEventListener("click", () => {
     if (inputElement.value.length > 0) {
-        
         const taskElement = document.createElement("div");
-            const task = document.createElement("span");
-            task.innerText = inputElement.value;
-            taskElement.appendChild(task);
-            taskElement.addEventListener("click", () => {
-                if (taskElement.classList.contains("completed")) {
-                    taskElement.classList.remove("completed");
-                } else {
-                    taskElement.classList.add("completed"); 
-                }
-            });
+
+        const task = document.createElement("span");
+        task.innerText = inputElement.value;
+        taskElement.appendChild(task);
+        inputElement.value = "";
+        taskElement.addEventListener("click", () => {
+            if (taskElement.style.textDecoration) {
+                taskElement.style = "none";
+           } else {
+                taskElement.style.textDecoration = "line-through";
+            }
+        });
+            
+        const removeButton = document.createElement("button");
+        removeButton.classList.add("remove-button");
+        removeButton.innerText = "🚫";
+        removeButton.addEventListener("click", removeTask);
+        taskElement.appendChild(removeButton);
         
-            const removeButton = document.createElement("button");
-            removeButton.classList.add("remove-button");
-            removeButton.innerText = "🚫";
-            removeButton.addEventListener("click", removeTask);
-            taskElement.appendChild(removeButton);
-        
-        tasksElement.appendChild(taskElement);
+        taskContainerElement.appendChild(taskElement);
     } else {
         alert("Please enter a task!");
     }
-}
 
+});
+
+// next 3 lines where written with help from the fruit shop tutorial video
 function removeTask() {
     const element = this.parentNode;
     element.parentNode.removeChild(element);
 }
 
-createList();
+
 
 
 //explanation
